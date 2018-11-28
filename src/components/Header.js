43b06a7741d01f10e14f4e6de0aa09024251aa3d
Aaron McKinney <amckinney@cbtnuggets.com>
2018-11-27 16:12:24 -0800

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>Aaron McKinney</h1>
+                <p>Husband, Father, Friend, Learner, Gamer and Lover of Burritos</p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: React.PropTypes.func,
+    timeout: React.PropTypes.bool
+}
+
+export default Header
